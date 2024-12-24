57c2c58384d5ea4ef246dd30a47c42eb6cbc730a
Yifei Yao <56761074+securityCFS@users.noreply.github.com>
2024-12-24 16:53:11 +0800

    Update mm interface for 1.0.2 distro of rsl_rl

--- rsl_rl/storage/__init__.py
+++ rsl_rl/storage/__init__.py
@@ -1,9 +1,4 @@
 #  Copyright 2021 ETH Zurich, NVIDIA CORPORATION
 #  SPDX-License-Identifier: BSD-3-Clause
 
-"""Implementation of transitions storage for RL-agent."""
-
 from .rollout_storage import RolloutStorage
-from .rollout_storage_mm import RolloutStorageMM
-
-__all__ = ["RolloutStorage", "RolloutStorageMM"]
