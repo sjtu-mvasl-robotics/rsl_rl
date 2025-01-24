correct the calculation for imitation lossss

rsl_rl/algorithms/mmppo.py
@@ -193,10 +193,13 @@ class MMPPO:
             # Imitation Entropy loss (optional, arxiv: 2409.08904)
             # This loss is calculated only when critic_ref_obs_batch is not None, otherwise 0.0
             # By default, we assume that ref_action_batch = critic_ref_obs_batch[0][:, ref_action_idx: num_actions + ref_action_idx] where ref_action_idx = 0
+            # Loss: \sum_i [\sqrt(2 * \pi * \sigma_i^2) + (mu_i - ref_action_i)^2 / (2 * \sigma_i^2)]
             if self.teacher_coef is not None and critic_ref_obs_batch is not None:
                 ref_action_batch = critic_ref_obs_batch[0][:, self.ref_action_idx: self.ref_action_idx + actions_batch.shape[-1]]
                 ref_action_mask = critic_ref_obs_batch[1].float()
-                imitation_loss = torch.sum(torch.sum((torch.square(mu_batch - ref_action_batch) / (2.0 * torch.square(sigma_batch) + 1e-5)), axis=-1) * ref_action_mask) / (ref_action_mask.sum() + 1e-5)
+                imitation_loss = torch.sum((
+                    (1 / actions_batch.shape[-1]) * (torch.sum((torch.square(mu_batch - ref_action_batch) / (2.0 * torch.square(sigma_batch) + 1e-5)), axis=-1) + torch.sum(torch.log(sigma_batch), axis=-1))
+                ) * ref_action_mask) / (ref_action_mask.sum() + 1e-5)
                 imitation_loss = self.teacher_coef * imitation_loss
             else:
                 imitation_loss = 0.0     