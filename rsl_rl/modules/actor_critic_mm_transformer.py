06f00daae5fcd7d2233b3df58eb5d5a309acab70
securityCFS <710244487@qq.com>
2025-03-20 16:43:58 +0800

    Update MM transformer to add debug transformer

--- rsl_rl/modules/actor_critic_mm_transformer.py
+++ rsl_rl/modules/actor_critic_mm_transformer.py
@@ -12,13 +12,17 @@ from typing import Optional, Tuple
 import time
 
 class ObservationEmbedding(nn.Module):
-    def __init__(self, num_obs, d_model, max_len=16):
+    def __init__(self, num_obs, d_model, max_len=16, apply_norm = False):
         super().__init__()
         self.d_model = d_model
         self.max_len = max_len
-        self.embedding = nn.Sequential(
-            nn.Linear(num_obs, d_model * max_len),
-        )
+        if apply_norm:
+            self.embedding = nn.Sequential(
+                nn.Linear(num_obs, d_model * max_len),
+                nn.LayerNorm(d_model * max_len),
+            )
+        else:
+            self.embedding = nn.Linear(num_obs, d_model * max_len)
         self.pos_embedding = nn.Embedding(max_len, d_model)
 
     def forward(self, x):
@@ -29,7 +33,7 @@ class ObservationEmbedding(nn.Module):
     
 
     
-class Transformer(nn.Module):
+class MMTransformer(nn.Module):
     def __init__(
             self,
             obs_size,
@@ -173,6 +177,94 @@ class Transformer(nn.Module):
         return x
     
 
+class Transformer(nn.Module):
+    '''
+        This class is for debugging only.
+        Do not use it for real PPO training.
+    '''
+    def __init__(
+            self,
+            obs_size,
+            ref_obs_size,
+            dim_out,
+            dim_model,
+            max_len = 128,
+            num_heads = 8,
+            num_layers = 4,
+            ffn_ratio = 4,
+            dropout = 0.0,
+            name = "",
+            ls_init_values = 1e-3,
+            **kwargs
+    ):
+        super().__init__()
+        self.name = name
+        self.obs_size = obs_size
+        self.ref_obs_size = ref_obs_size
+        if kwargs:
+            print(f"Transformer.__init__ got unexpected arguments, which will be ignored: {kwargs.keys()}")
+        self.obs_embedding = ObservationEmbedding(obs_size + ref_obs_size, dim_model, max_len)
+        # self.obs_embedding = ObservationEmbedding(obs_size + ref_obs_size, dim_model, max_len)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim_model))
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=dim_model,
+            nhead=num_heads,
+            dim_feedforward=dim_model * ffn_ratio,
+            dropout=dropout,
+            activation='gelu',
+            batch_first=True
+        )
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers, norm=nn.LayerNorm(dim_model))
+        self.fc = nn.Sequential(
+            nn.LayerNorm(dim_model),
+            nn.Linear(dim_model, dim_out),
+        )
+
+    def forward(
+        self, 
+        obs: torch.Tensor, 
+        ref_obs: Optional[Tuple[torch.Tensor, torch.Tensor]] = None
+    ) -> torch.Tensor:
+        """
+        Forward pass for the multi-modality transformer.
+
+        Args:
+            obs (torch.Tensor): Observation tensor of shape (B, seq_len_obs, dim_in).
+            ref_obs (Optional[Tuple[torch.Tensor, torch.Tensor]]): 
+                A tuple containing:
+                - ref_obs_tensor (torch.Tensor): Reference observations of shape (B, seq_len_ref_obs, dim_in).
+                - ref_obs_mask (torch.Tensor): Mask tensor of shape (B,) indicating the presence of ref_obs.
+
+        Returns:
+            torch.Tensor: Output tensor after transformer and fully connected layer.
+        """
+        embeddings = []
+
+        if ref_obs is not None:
+            ref_obs_tensor, _ = ref_obs  # Unpack the tuple
+        else:
+            ref_obs_tensor = torch.zeros(self.ref_obs_size, device=obs.device).unsqueeze(0).expand(obs.size(0), -1)  # Shape: (B, seq_len_ref_obs)
+        obs = torch.cat([obs, ref_obs_tensor], dim=1)  # Concatenate obs and ref_obs along the last dimension
+        obs_emb = self.obs_embedding(obs)  # Shape: (B, seq_len_obs, dim_model)
+        cls_emb = self.cls_token.expand(obs.size(0), -1, -1)  # Shape: (B, 1, dim_model)
+        
+        obs_emb = torch.cat([cls_emb, obs_emb], dim=1)  # Shape: (B, seq_len_obs + 2, dim_model)
+        embeddings.append(obs_emb)
+            
+        x = torch.cat(embeddings, dim=1)  # Shape: (B, total_seq_len, dim_model)
+        # -------------------
+        # Transformer processing
+        # -------------------
+        x = self.transformer(x)  # Transformer expects shape (S, B, E)
+        x = x[:, 0, :]  # Take the first token's output: Shape (B, dim_model)
+
+        # -------------------
+        # Final fully connected layer
+        # -------------------
+        x = self.fc(x)  # Shape: (B, output_dim)
+        # x = self.out_ls(x)
+        return x
+
 
 class ActorCriticMMTransformer(nn.Module):
     is_recurrent = False
@@ -191,8 +283,8 @@ class ActorCriticMMTransformer(nn.Module):
             **kwargs
     ):
         super().__init__()
-        self.actor = Transformer(num_actor_obs, num_actor_ref_obs, num_actions, dim_model, max_len=max_len, num_heads=num_heads, num_layers=num_layers, name="actor", **kwargs)
-        self.critic = Transformer(num_critic_obs, num_critic_ref_obs, 1, dim_model, max_len=max_len, num_heads=num_heads, num_layers=num_layers, name="critic", **kwargs) # 1 for value function
+        self.actor = MMTransformer(num_actor_obs, num_actor_ref_obs, num_actions, dim_model, max_len=max_len, num_heads=num_heads, num_layers=num_layers, name="actor", **kwargs)
+        self.critic = MMTransformer(num_critic_obs, num_critic_ref_obs, 1, dim_model, max_len=max_len, num_heads=num_heads, num_layers=num_layers, name="critic", **kwargs) # 1 for value function
         print(f"Actor Transformer: {self.actor}")
         print(f"Critic Transformer: {self.critic}")
         # Action noise
